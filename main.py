8bb7ee90127702d1a6117c4480d35385c6eb0216
cauecrb <crburgardt@gmail.com>
2021-02-02 16:05:43 -0300

    criando decoder e juntando a arquitetura
    criando o decoder e juntando a arquitetura

--- main.py
+++ main.py
@@ -89,8 +89,10 @@ outputs = [[vocab_size_pt - 2] + tokenizer_pt.encode(sentence) + [vocab_size_pt 
 for _ in range(5):
     print(outputs[random.randint(0, len(outputs) -1 )])
 
-#remoção de sentençãs muito longas, para diminuir o tempo de treinamento
-max_length = 25
+max_length = 60
+
+'''#remoção de sentençãs muito longas, para diminuir o tempo de treinamento
+
 idx_to_remove = [count for count, sent in enumerate(inputs) if len(sent) > max_length]
 
 print(len(idx_to_remove))
@@ -107,7 +109,7 @@ for idx in reversed(idx_to_remove):
 
 print(len(inputs))
 print(len(outputs))
-
+'''
 
 #preenxendo as frases com 0 para deixar todas do mesmo tamanho
 inputs = tf.keras.preprocessing.sequence.pad_sequences(inputs, value=0, padding='post', maxlen=max_length)
@@ -149,17 +151,17 @@ class PositionalEncoding(layers.Layer):
         pos_encoding = angles[np.newaxis, ...]
         return inputs + tf.cast(pos_encoding, tf.float32)
 
-    # mecanismo de atenção
-    def scaled_dot_product_attention(queries, keys, values, mask):
-        product = tf.matmul(queries, keys, transpose_b=True)
-        keys_dim = tf.cast(tf.shape(keys)[-1], tf.float32)
-        scaled_product = product / tf.math.sqrt(keys_dim)
+# mecanismo de atenção
+def scaled_dot_product_attention(queries, keys, values, mask):
+    product = tf.matmul(queries, keys, transpose_b=True)
+    keys_dim = tf.cast(tf.shape(keys)[-1], tf.float32)
+    scaled_product = product / tf.math.sqrt(keys_dim)
 
-        if mask is not None:
-            scaled_product += (mask * -1e9) # 0.0000000001
+    if mask is not None:
+        scaled_product += (mask * -1e9) # 0.0000000001
 
-        attention = tf.matmul(tf.nn.softmax(scaled_product, axis=-1), values)
-        return attention
+    attention = tf.matmul(tf.nn.softmax(scaled_product, axis=-1), values)
+    return attention
 print('criado mecanismo de atenção')
 
 #mult-head attention
@@ -199,7 +201,7 @@ class MultiHeadAttention(layers.Layer):
 
         attention = scaled_dot_product_attention(queries, keys, values, mask)
 
-        attention = tf.transpose(attention, perm[0, 2, 1, 3])
+        attention = tf.transpose(attention, perm=[0, 2, 1, 3])
 
         concat_attention = tf.reshape(attention, shape=(batch_size, -1, self.d_model))
 
@@ -207,3 +209,176 @@ class MultiHeadAttention(layers.Layer):
         return outputs
 
     print('criado camada de mult-head attention')
+
+#encoder
+class EncoderLayer(layers.Layer):
+    def __init__(self, FFN_units, nb_proj, dropout_rate):
+        super(EncoderLayer, self).__init__()
+        self.FFN_units = FFN_units
+        self.nb_proj = nb_proj
+        self.dropout_rate = dropout_rate
+
+    def build(self, input_shape):
+        self.d_model = input_shape[-1]
+
+        self.multi_head_attention = MultiHeadAttention(self.nb_proj)
+        self.dropout_1 = layers.Dropout(rate=self.dropout_rate)
+        self.norm_1 = layers.LayerNormalization(epsilon=1e-6)
+
+        self.dense_1 = layers.Dense(units=self.FFN_units, activation='relu')
+        self.dense_2 = layers.Dense(units=self.FFN_units, activation='relu')
+        self.dropout_2 = layers.Dropout(rate=self.dropout_rate)
+
+        self.norm_2 = layers.LayerNormalization(epsilon=1e-6)
+
+    def call(self, inputs, mask, training):
+        attention = self.multi_head_attention(inputs, inputs, inputs, mask)
+        attention = self.dropout_1(attention, training = training)
+        attention = self.norm_1(attention + inputs)
+
+        outputs = self.dense_1(attention)
+        outputs = self.dense_2(outputs)
+        outputs = self.dropout_2(outputs, training = training)
+        outputs = self.norm_2(outputs + attention)
+
+        return outputs
+
+    print("criada o encoder")
+
+class Encoder(layers.Layer):
+    def __init__(self,
+                 nb_layers,
+                 FFN_units,
+                 nb_proj,
+                 dropout_rate,
+                 vocab_size,
+                 d_model,
+                 name="encoder"):
+        super(Encoder, self).__init__(name=name)
+        self.nb_layers = nb_layers
+        self.d_model = d_model
+        
+        self.embedding = layers.Embedding(vocab_size, d_model)
+        self.pos_encoding = PositionalEncoding()
+        self.dropout = layers.Dropout(rate=dropout_rate)
+        self.enc_layer = [EncoderLayer(FFN_units,nb_proj, dropout_rate) for _ in range(nb_layers) ]
+
+    def call(self, inputs, mask, training):
+        outputs = self.embedding(inputs)
+        outputs *= tf.math.sqrt(tf.cast(self.d_model, tf.float32))
+        outputs = self.pos_encoding(outputs)
+        outputs = self.dropout(outputs, training)
+
+        for i in range(self.nb_layers):
+            outputs = self.enc_layer[i](outputs, mask, training)
+
+        return outputs
+print("criado encoder 2")
+
+#decoder 1:  segundo o diagrama da arquietura transformer
+#nececitamos duas camadas de multihead attention
+
+class DecoderLayer(layers.Layer):
+    def __init__(self, FFN_units, nb_proj, dropout_rate):
+        super(DecoderLayer, self).__init__()
+        self.FFN_units = FFN_units
+        self.nb_proj = nb_proj
+        self.dropout_rate = dropout_rate
+
+    def build(self, input_shape):
+        self.d_model = input_shape[-1]
+
+        self.multi_head_attention_1 = MultiHeadAttention(self.nb_proj)
+        self.dropout_1 = layers.Dropout(rate=self.dropout_rate)
+        self.norm_1 = layers.LayerNormalization(epsilon=1e-6)
+
+        self.multi_head_attention_2 = MultiHeadAttention(self.nb_proj)
+        self.dropout_2 = layers.Dropout(rate=self.dropout_rate)
+        self.norm_2 = layers.LayerNormalization(epsilon=1e-6)
+
+        self.dense_1 = layers.Dense(units=self.FFN_units, activation='relu')
+        self.dense_2 = layers.Dense(units=self.FFN_units, activation='relu')
+        self.dropout_3 = layers.Dropout(rate=self.dropout_rate)
+        self.norm_3 = layers.LayerNormalization(epsilon=1e-6)
+
+
+    def call(self, inputs, enc_outputs, mask_1, mask_2, training):
+        attention = self.multi_head_attention_1(inputs, inputs, inputs, mask_1)
+        attention = self.dropout_1(attention, training)
+        attention = self.norm_1(attention + inputs)
+
+        attention_2 = self.multi_head_attention_2(attention, enc_outputs, enc_outputs, mask_2)
+        attention_2 = self.dropout_2(attention_2, training)
+        attention_2 = self.norm_2(attention_2 + attention)
+
+        outputs = self.dense_1(attention_2)
+        outputs = self.dense_2(outputs)
+        outputs = self.dropout_3(outputs, training)
+        outputs = self.norm_3(outputs + attention_2)
+
+        return outputs
+
+print("criação decoder 1")
+
+class Decoder(layers.Layer):
+    def __init__(self,
+                 nb_layers,
+                 FFN_units,
+                 nb_proj,
+                 dropout_rate,
+                 vocab_size,
+                 d_model,
+                 name="decoder"):
+        super(Decoder, self).__init__(name=name)
+        self.d_model = d_model
+        self.nb_layers = nb_layers
+
+        self.embedding = layers.Embedding(vocab_size, d_model)
+        self.pos_encoding = PositionalEncoding()
+        self.dropout = layers.Dropout(rate=dropout_rate)
+
+        self.dec_layers = [DecoderLayer(FFN_units, nb_proj, dropout_rate) for i in range(nb_layers)]
+
+    def call(self, inputs, enc_outputs, mask_1, mask_2, training):
+        outputs = self.embedding(inputs)
+        outputs *= tf.math.sqrt(tf.cast(self.d_model, tf.float32))
+        outputs = self.pos_encoding(outputs)
+        outputs = self.dropout(outputs, training)
+
+        for i in range(self.nb_layers):
+            outputs = self.dec_layers[i](outputs, enc_outputs, mask_1, mask_2, training)
+
+        return outputs
+
+print("decoder 2")
+
+#juntando o mdelo transformer
+
+class Transformer(tf.keras.Model):
+    def __init__(self,
+                 vocab_size_enc,
+                 vocab_size_dec,
+                 d_model,
+                 nb_layers,
+                 FFN_units,
+                 nb_proj,
+                 dropout_rate,
+                 name="transformer"):
+        super(Transformer, self).__init__(name=name)
+
+        self.encoder = Encoder(nb_layers, FFN_units, nb_proj,
+                               dropout_rate,vocab_size_enc, d_model)
+        self.decoder= Decoder(nb_layers, FFN_units, nb_proj,
+                              dropout_rate,vocab_size_dec, d_model)
+        self.last_linear = layers.Dense(units=vocab_size_dec, name='lin_output')
+
+    def create_padding_mask(self, seq):
+        mask = tf.cast(tf.math.equal(seq, 0), tf.float32)
+        return mask[:, tf.newaxis, tf.newaxis, :]
+
+    def create_look_ahead_mask(self, seq):
+        seq_len = tf.shape(seq)[1]
+        look_ahead_mask = 1 - tf.linalg.band_part(tf.ones((seq_len,seq_len)), -1, 0)
+        return look_ahead_mask
+
+    def call(self, enc_inputs, dec_inputs, training):
